6977f9ed58b7166f2fbbc08099e3a89fbada9224
Jim Paulo Ovejera <f360.jovejera@gmail.com>
2015-07-22 08:58:17 +0800

    Added HttpUrlConnection class to send JSON string

--- app/src/main/java/jim/gibx/gibco/guevent/com/gibxadmindailyreport/ButtonHandler.java
+++ app/src/main/java/jim/gibx/gibco/guevent/com/gibxadmindailyreport/ButtonHandler.java
@@ -1,10 +1,19 @@
 package jim.gibx.gibco.guevent.com.gibxadmindailyreport;
 
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ButtonHandler implements View.OnClickListener {
 
@@ -24,8 +33,47 @@ public class ButtonHandler implements View.OnClickListener {
                 txtMLMFToday.getText().toString(),
                 txtMLMIToday.getText().toString() );
         Gson gson = new Gson();
-
+        sendJson(gson.toJson(today));
         Log.i("JSON: " + gson.toJson(today), "onclick");
+    }
+
+    private void sendJson(String json){
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        HttpURLConnection httpCon;
+        String url = "http://yeswecare.16mb.com/upload/";
+        try{
+            httpCon = (HttpURLConnection) ((new URL(url).openConnection()));
+           // httpCon.setDoOutput(true);
+            //httpCon.setRequestProperty("Content-Type", "application/json");
+           // httpCon.setRequestProperty("Accept", "application/json");
+            httpCon.setRequestProperty("User-Agent","Mozilla/5.0 ( compatible ) ");
+            httpCon.setRequestProperty("Accept","*/*");
+            httpCon.setRequestMethod("GET");
+            httpCon.connect();
+
+            OutputStream os = httpCon.getOutputStream();
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            bw.write("today=" + json);
+            bw.close();
+
+            os.close();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(httpCon.getInputStream(), "UTF-8"));
+            String line = null;
+            StringBuilder builder = new StringBuilder();
+            while((line = reader.readLine()) != null){
+                builder.append(line);
+            }
+            reader.close();
+            Log.i("RESULT: " + builder.toString(), "res");
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
